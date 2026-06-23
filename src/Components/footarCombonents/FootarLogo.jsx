545cbe79ac0ae8543ca8bdf83155a6ba8b779230
yuossef151 <yuossefgaafar07@gmail.com>
2026-06-23 14:25:30 +0300

    first commit

--- src/Components/footarCombonents/FootarLogo.jsx
+++ src/Components/footarCombonents/FootarLogo.jsx
@@ -3,8 +3,8 @@ export default function FootarLogo() {
   return (
     <>
       <div className="lg:w-[24%]">
-        <p className="text-white">قطع غيار السيارات</p>
-        <p className="text-[#b1b0b0] ">منصة احترافية متخصصة في بيع قطع غيار السيارات بجودة عالية وأسعار منافسة. نخدم الأفراد والورش ومراكز الصيانة.</p>
+       <img className="h-28 w-28 " src={`${import.meta.env.BASE_URL}img/logo2.png`}alt="" />
+        <p className="text-[#b1b0b0] pt-4">منصة احترافية متخصصة في بيع قطع غيار السيارات بجودة عالية وأسعار منافسة. نخدم الأفراد والورش ومراكز الصيانة.</p>
       </div>
     </>
   )
--- src/Components/headerComponents/Logo.jsx
+++ src/Components/headerComponents/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <>
       <div className=" h-full">
-        <img className="h-28 w-28 " src={`${import.meta.env.BASE_URL}img/logo.png`}alt="" />
+        <img className="h-28 w-28 bg-white" src={`${import.meta.env.BASE_URL}img/logo.png`}alt="" />
       </div>
     </>
   );
